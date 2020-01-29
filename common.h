3230970e00134b9b6c48cb5b604073230cc89e9c
Jason Cater <jasondcater@gmail.com>
2020-01-29 14:23:48 -0800

    Adding audio_utils.c and binary_utils.c.

--- common.h
+++ common.h
@@ -1,14 +1,24 @@
 #include <stdint.h>
 
 // msg_processing.c
-void msg_to_baudot(char msg[], int *p_msg_buffer);
-void baudot_to_fsk(int *p_msg_buffer, int *p_tx_buffer);
+void msg_to_baudot(uint8_t msg[], uint32_t *p_msg_buffer);
+void baudot_to_fsk(uint32_t *p_msg_buffer, uint32_t *p_tx_buffer);
+
+// binary_utils.c
+void reverse_endian(uint64_t data_size, uint8_t width, uint8_t *data);
+uint32_t is_big_endian();
+
+// audio_utils.c
+void write_audio_to_file(
+  uint8_t *audio_buffer,
+  uint32_t data_size,
+  uint32_t sample_rate,
+  uint16_t num_channels,
+  uint16_t bit_depth
+);
 
 // audio.c
 uint32_t open_audio();
 void close_audio();
 uint32_t generate_sine(double hertz);
 void write_audio_file();
-
-// audio_utils.c
-void write_audio_to_file();
