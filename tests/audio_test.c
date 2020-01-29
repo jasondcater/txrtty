c2b17f3439323abad3f47e5a904fe6f121a88bdf
Jason Cater <jasondcater@gmail.com>
2020-01-29 14:24:26 -0800

    Adding the audio utils to the tests.

--- tests/audio_test.c
+++ tests/audio_test.c
@@ -2,9 +2,11 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 
 void test_open_audio(){
-  open_audio();
+  uint32_t status = open_audio();
+  if(status) printf("test_audio_open() failed. Wrong status code.\n");
 }
 
 void test_close_audio(){
@@ -12,12 +14,15 @@ void test_close_audio(){
 }
 
 void test_generate_sine(){
-  generate_sine(2125.0);
+  uint32_t status = generate_sine(2125.0);
+  if(status) printf("test_generate_sine() failed. Wrong status code.\n");
+
+  write_audio_file();
 }
 
 int main(int argc, char *argv[]){
-  test_open_audio();
-  test_close_audio();
+  //test_open_audio();
+  //test_close_audio();
   test_generate_sine();
   exit(0);
 }
