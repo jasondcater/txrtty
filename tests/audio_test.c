87fb20d1eadefb55566d4c80235cd17958e071ff
Jason Cater <jasondcater@gmail.com>
2020-01-30 12:22:54 -0800

    Finishing the test for generating a sine wave.

--- tests/audio_test.c
+++ tests/audio_test.c
@@ -5,8 +5,8 @@
 #include <stdint.h>
 
 void test_open_audio(){
-  uint32_t status = open_audio();
-  if(status) printf("test_audio_open() failed. Wrong status code.\n");
+  int64_t status = open_audio();
+  if(status < 0) printf("test_audio_open() failed. Wrong status code.\n");
 }
 
 void test_close_audio(){
@@ -14,15 +14,32 @@ void test_close_audio(){
 }
 
 void test_generate_sine(){
-  uint32_t status = generate_sine(2125.0);
-  if(status) printf("test_generate_sine() failed. Wrong status code.\n");
 
-  write_audio_file();
+  /*
+   * Here we are making "status" do double duty. If the status is less than zero
+   * then we can use it as a status code. Otherwise, "status" will be the
+   * address of the audio buffer that was filled. As you can see we do some
+   * casting of "status" to output the individual bytes in the audio buffer.
+   */
+  int64_t status = generate_sine(2125.0);
+  if(status < 0) printf("test_generate_sine() failed. Wrong status code.\n");
+
+  // This data represents the first 20 bytes of a generated sine wave.
+  uint8_t sample_data[] = {
+    29, 28, 18, 54, 222, 75, 214, 91, 190, 100, 231, 101, 59, 95, 60, 81, 255,
+    60, 17, 36 
+  };
+
+  for(uint8_t a = 0; a < 20; a++){
+    if((uint8_t)*((int8_t *)status+a) != sample_data[a]){
+      printf("test_generate_sine() failed. Generated data does not match.\n");
+    }
+  }
 }
 
-int main(int argc, char *argv[]){
-  //test_open_audio();
-  //test_close_audio();
+int main(){
+  test_open_audio();
+  test_close_audio();
   test_generate_sine();
   exit(0);
 }
