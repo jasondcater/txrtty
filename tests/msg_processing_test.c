74e04662986ed43880e117adf88b093cdf62c67a
Jason Cater <jasondcater@gmail.com>
2020-02-22 20:13:33 -0600

    Changing the variables to fixed widths.

--- tests/msg_processing_test.c
+++ tests/msg_processing_test.c
@@ -1,11 +1,12 @@
 #include "../common.h"
 
 #include <stdlib.h>
+#include <stdint.h>
 #include <stdio.h>
 
 void test_msg_to_baudot(){
-  int *p_msg_buffer;
-  int msg_buffer[BUFSIZ];
+  uint32_t *p_msg_buffer;
+  uint32_t msg_buffer[BUFSIZ];
 
   // This would be the message you would want to send.
   char msg[13] = "spcstn-kj6zrf";
@@ -14,7 +15,7 @@ void test_msg_to_baudot(){
    * This should be the message converted into our Baudot code. The first index
    * is reserved as a count of elements of the message.
    */
-  int baudot[] = {
+  uint32_t baudot[] = {
     20, 127, 82, 55, 59, 82, 7, 26, 110, 14, 127, 122, 107, 110, 87, 127, 70,
     42, 91, 35
   };
@@ -24,7 +25,7 @@ void test_msg_to_baudot(){
 
   size_t baudot_size = sizeof(baudot) / sizeof(int);
 
-  int a;
+  uint32_t a;
   for(a = 0; a < baudot_size; a++){
     if(baudot[a] != msg_buffer[a]){
       printf("msg_to_baudot() failed\n");
@@ -34,19 +35,19 @@ void test_msg_to_baudot(){
 }
 
 void test_baudot_to_fsk(){
-  int *p_msg_buffer;
-  int msg_buffer[BUFSIZ];
-  int *p_tx_buffer;
-  int tx_buffer[BUFSIZ][8];
+  uint32_t *p_msg_buffer;
+  uint32_t msg_buffer[BUFSIZ];
+  uint32_t *p_tx_buffer;
+  uint32_t tx_buffer[BUFSIZ][8];
 
   // This is the baudot encoding for "spcstn-kj6zrf".
-  int baudot[] = {
+  uint32_t baudot[] = {
     20, 127, 82, 55, 59, 82, 7, 26, 110, 14, 127, 122, 107, 110, 87, 127, 70,
     42, 91, 35
   };
 
   // This is the fsk binary representation of the baudot code above.
-  int fsk[][8] = {
+  uint32_t fsk[][8] = {
     {20, 0, 0, 0, 0, 0, 0, 0},
     {2125, 2295, 2295, 2295, 2295, 2295, 2295, 2295},
     {2125, 2295, 2125, 2295, 2125, 2125, 2295, 2125},
@@ -72,14 +73,14 @@ void test_baudot_to_fsk(){
   p_msg_buffer = &msg_buffer[0];
   p_tx_buffer = &tx_buffer[0][0];
 
-  int a;
+  uint32_t a;
   for(a = 0; a < baudot[0]; a++){
     p_msg_buffer[a] = baudot[a];
   }
 
   baudot_to_fsk(p_msg_buffer, p_tx_buffer);
 
-  int b, c;
+  uint32_t b, c;
   for(b = 0; b < baudot[0]; b++){
     for(c = 0; c < 8; c++){
       if(fsk[b][c] != tx_buffer[b][c]){
