77dca5555c256b2d7ae37f67224898a6ee6cf0c8
Jason Cater <jasondcater@gmail.com>
2020-03-01 17:27:44 -0600

    Pulling the last line of a log file as the message.

--- txrtty.c
+++ txrtty.c
@@ -2,23 +2,25 @@
 
 #include <pthread.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
-int running = 1;
-int has_data = 0; // Lets the system know there is data ready for transmission.
+uint32_t running = 1;
+uint32_t has_data = 0; // Lets the system know there is data ready for transmission.
 
 // Commands
-char cmd[BUFSIZ];
-const char quit[] = "quit";
-const char help[] = "help";
+int8_t cmd[BUFSIZ];
+const uint8_t quit[] = "quit";
+const uint8_t help[] = "help";
 
 // Data buffers
-int *p_msg_buffer;
-int msg_buffer[BUFSIZ];
-int *p_tx_buffer;
-int tx_buffer[BUFSIZ][8];
-
+uint32_t *p_msg_buffer;
+uint32_t msg_buffer[BUFSIZ];
+uint32_t *p_tx_buffer;
+uint32_t tx_buffer[BUFSIZ][8];
+/*
 void *input_loop(void *thread_id){
   while(running){
 
@@ -43,20 +45,72 @@ void *input_loop(void *thread_id){
 
   pthread_exit(0);
 }
+*/
+
+void *input_loop(void *thread_id){
+  FILE *fd;
+  fd = fopen((char *)"tests/bin/tests.log", "rb");
+
+  if(fd == NULL){
+    printf("Cannot open log file.");
+    running = 0;
+    pthread_exit(&errno);
+  }
+
+  while(running){
+
+    /*
+     * Loop backwards from the end of the file looking for newline chars that
+     * demarcate the lines. We seek backwards until we have the second newline
+     * char. This indicates that we have an index range for the last line. Also,
+     * we check that we are still within the size of the file.
+     */
+    int64_t char_count = -1;
+    int64_t newline_count = 0;
+
+    fseek(fd, 0, SEEK_END);
+    int64_t file_size = ftell(fd);
+
+    // Seek the file discriptor to the end of the file.
+    fseek(fd, char_count, SEEK_END);
+
+    while(newline_count < 2 && abs(char_count) <= file_size){
+      char c[] = {fgetc(fd)};
+
+      if(strcmp("\n", c) == 0){
+        newline_count++;
+      }
+      else{
+        // Add the char to the message buffer.
+        msg_buffer[abs(char_count)] = c[0];
+      }
+
+      char_count--;
+      fseek(fd, char_count, SEEK_END);
+    }
+
+    // Add the message size to the message buffer.
+    msg_buffer[0] = abs(char_count);
+  }
+
+  if(fd != NULL) fclose(fd);
+  pthread_exit(0);
+}
 
 void transmit_blank(){
   int bit = 0;
   while(bit < 8){
-    write_wave(blank[bit]);
+    //write_wave(blank[bit]);
     bit++;
   }
 }
 
 void transmit_message(){
   int bit = 8;
-  int bit_count = *(tx_buffer) * 8; // Total bits to transmit.
+  //int bit_count = *(tx_buffer) * 8; // Total bits to transmit.
+  int bit_count = 10;
   while(bit < bit_count){
-    write_wave(*(tx_buffer + bit));
+    //write_wave(*(tx_buffer + bit));
     bit++;
   }
 }
@@ -90,25 +144,26 @@ int main(int argc, char *argv[]){
 
   if(open_audio() != 0){
     perror("open_audio() error");
-    exit(1);
+    exit(errno);
   }
 
   // Spawn the threads.
   if(
-    pthread_create(&input_thread, NULL, input_loop, (void *)t) != 0 ||
-    pthread_create(&tx_thread, NULL, tx_loop, (void *)t) != 0
+    pthread_create(&input_thread, NULL, input_loop, (void *)t) != 0 //||
+//    pthread_create(&tx_thread, NULL, tx_loop, (void *)t) != 0
   ){
     perror("pthread_create() error");
-    exit(1);
+    exit(errno);
   }
 
   // Wait for the threads to exit before finishing.
+
   if(
-    pthread_join(input_thread, &status) != 0 ||
-    pthread_join(tx_thread, &status) != 0
+    pthread_join(input_thread, &status) != 0 //||
+  //  pthread_join(tx_thread, &status) != 0
   ){
     perror("pthread_join() error");
-    exit(3);
+    exit(errno);
   }
 
   exit(0);
